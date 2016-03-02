0c20d700d86d362d1e679601b8642f923d80f255
Tal Leming <tal@typesupply.com>
2016-03-02 13:30:13 -0500

    BaseComponent and BaseAnchor.

--- Lib/fontParts/objects/abstract/__init__.py
+++ Lib/fontParts/objects/abstract/__init__.py
@@ -990,3 +990,155 @@ class BaseBPoint(BaseObject):
         Duplicate this point
         """
 
+
+class BaseComponent(BaseObject):
+
+    # ----------
+    # Attributes
+    # ----------
+    """
+    - selected
+    - index
+    - baseGlyph
+    - offset
+    - scale
+    - transformation
+    """
+
+    def __repr__(self):
+        pass
+
+    # ----
+    # Pens
+    # ----
+
+    def draw(self, pen):
+        pass
+
+    def drawPoints(self, pen):
+        pass
+
+    # ---------------
+    # Transformations
+    # ---------------
+
+    def transform(matrix):
+        """
+        Transform this component. (use a Transform matrix object from ``robofab.transform``)
+
+        - don't require a matrix object. accept a tuple.
+        """
+
+    def move((x, y)):
+        """
+        Move the component.
+        """
+
+    def scale((x, y), center=(0, 0)):
+        """
+        Scale the component.
+        """
+
+    def rotate(angle, offset=None):
+        """
+        Rotate the component.
+
+        - the center should be definable.
+        """
+
+    def skew(angle, offset=None):
+        """
+        Skew the component.
+
+        - the center should be definable.
+        """
+
+    # ----
+    # Misc
+    # ----
+
+    def decompose(self):
+        pass
+
+    def round(self):
+        pass
+
+    def copy(self, aParent=None):
+        """
+        Duplicate this component
+        """
+
+
+class BaseAnchor(BaseObject):
+
+    # ----------
+    # Attributes
+    # ----------
+    """
+    - selected
+    - index
+    - x
+    - y
+    - position (is this needed?)
+    - mark
+    """
+
+    def __repr__(self):
+        pass
+
+    # ----
+    # Pens
+    # ----
+
+    def draw(self, pen):
+        pass
+
+    def drawPoints(self, pen):
+        pass
+
+    # ---------------
+    # Transformations
+    # ---------------
+
+    def transform(matrix):
+        """
+        Transform this anchor. (use a Transform matrix object from ``robofab.transform``)
+
+        - don't require a matrix object. accept a tuple.
+        """
+
+    def move((x, y)):
+        """
+        Move the anchor.
+        """
+
+    def scale((x, y), center=(0, 0)):
+        """
+        Scale the anchor.
+        """
+
+    def rotate(angle, offset=None):
+        """
+        Rotate the anchor.
+
+        - the center should be definable.
+        """
+
+    def skew(angle, offset=None):
+        """
+        Skew the anchor.
+
+        - the center should be definable.
+        """
+
+    # ----
+    # Misc
+    # ----
+
+    def round(self):
+        pass
+
+    def copy(self, aParent=None):
+        """
+        Duplicate this anchor
+        """
