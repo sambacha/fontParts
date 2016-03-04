03e236c53277ef9d994b5dc96a1c93f0fd795547
Tal Leming <tal@typesupply.com>
2016-03-04 10:51:08 -0500

    bPoint.

--- Lib/fontParts/objects/abstract/bPoint.py
+++ Lib/fontParts/objects/abstract/bPoint.py
@@ -4,61 +4,99 @@ class BaseBPoint(BaseObject):
     # ----------
     # Attributes
     # ----------
-    """
-    - selected
-    - index
-    - anchor
-    - bcpIn
-    - bcpOut
-    - type
-    """
 
     def __repr__(self):
+        pass
+
+    anchor = dynamicProperty("anchor", "The anchor point.")
+
+    def _get_anchor(self):
+        pass
+
+    def _set_anchor(self, value):
+        pass
+
+    bcpIn = dynamicProperty("bcpIn", "The incoming off curve.")
+
+    def _get_bcpIn(self):
+        pass
+
+    def _set_bcpIn(self, value):
+        pass
+
+    bcpOut = dynamicProperty("bcpOut", "The outgoing off curve.")
+
+    def _get_bcpOut(self):
+        pass
+
+    def _set_bcpOut(self, value):
+        pass
+
+    type = dynamicProperty("type", "The bPoint type.")
+
+    def _get_type(self):
+        pass
+
+    def _set_type(self, value):
+        pass
+
+    # --------------
+    # Identification
+    # --------------
+
+    index = dynamicProperty("index", "The index of the bPoint within the ordered list of the parent contour's bPoints. None if the bPoint does not belong to a contour.")
+
+    def _get_index(self):
         pass
 
     # ---------------
     # Transformations
     # ---------------
 
-    def transform(matrix):
+    def transform(self, matrix):
         """
-        Transform this point. (use a Transform matrix object from ``robofab.transform``)
-
-        - don't require a matrix object. accept a tuple.
-        """
-
-    def move((x, y)):
-        """
-        Move the point.
+        Transform the bPoint with the transformation matrix.
+        The matrix must be a tuple defining a 2x2 transformation
+        plus offset, aka Affine transform.
         """
 
-    def scale((x, y), center=(0, 0)):
+    def move(self, value):
         """
-        Scale the point.
-        """
-
-    def rotate(angle, offset=None):
-        """
-        Rotate the point.
-
-        - the center should be definable.
+        Move the bPoint by value. Value must
+        be a tuple defining x and y values.
         """
 
-    def skew(angle, offset=None):
+    def scale(self, value, center=None):
         """
-        Skew the point.
+        Scale the bPoint by value. Value must be a
+        tuple defining x and y values.
 
-        - the center should be definable.
+        center defines the (x, y) point at which the
+        scale should originate. The default is (0, 0).
         """
 
+    def rotate(self, angle, offset=None):
+        """
+        Rotate the bPoint by angle.
+
+        XXX define angle parameters.
+        XXX is anything using offset?
+        XXX it should be possible to define the center point for the rotation.
+        """
+
+    def skew(self, angle, offset=None):
+        """
+        Skew the bPoint by angle.
+
+        XXX define angle parameters.
+        XXX is anything using offset?
+        XXX it should be possible to define the center point for the skew.
+        """
     # ----
     # Misc
     # ----
 
     def round(self):
-        pass
-
-    def copy(self, aParent=None):
         """
-        Duplicate this point
+        Round coordinates.
         """
