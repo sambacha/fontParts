d6cae982a221a7acee7e3769fc83a5a50e1bf4eb
Tal Leming <tal@typesupply.com>
2016-03-04 13:48:43 -0500

    Clean up, documentation, new base dictionary.

--- Lib/fontParts/objects/abstract/base.py
+++ Lib/fontParts/objects/abstract/base.py
@@ -1,11 +1,19 @@
+# -------------------
+# Universal Exception
+# -------------------
+
 class FontPartsError(Exception): pass
 
 
+# ------------
+# Base Objects
+# ------------
+
 class BaseObject(object):
-    """
-    A base object for everything.
-    I'm not sure how this will work with subclassing.
-    """
+
+    # ----------
+    # Exceptions
+    # ----------
 
     def raiseNotImplementedError(self):
         """
@@ -13,6 +21,10 @@ class BaseObject(object):
         the base classes. So, it's here for convenience.
         """
         raise FontPartsError("The {className} subclass does not implement this method.".format(className=self.__class__.__name))
+
+    # ---------------------
+    # Environment Fallbacks
+    # ---------------------
 
     def update(self):
         """
@@ -27,29 +39,107 @@ class BaseObject(object):
         """
         self.raiseNotImplementedError()
 
-    def getParent(self):
-        """
-        - this is not sufficient anymore
-        """
-
     # ----
     # Math
     # ----
 
     def __mul__(self, factor):
-        pass
+        self.raiseNotImplementedError()
 
     __rmul__ = __mul__
 
     def __div__(self, factor):
-        pass
+        self.raiseNotImplementedError()
 
     def __add__(self, other):
-        pass
+        self.raiseNotImplementedError()
 
     def __sub__(self, other):
-        pass
+        self.raiseNotImplementedError()
 
+
+class BaseDict(BaseObject):
+
+    def fromkeys(self, keys):
+        self.raiseNotImplementedError()
+
+    def len(self):
+        return len(self.keys())
+
+    def keys(self):
+        return [k for k, v in self.items()]
+
+    def items(self):
+        self.raiseNotImplementedError()
+
+    def values(self):
+        return [v for k, v in self.items()]
+
+    def __contains__(self, key):
+        self.raiseNotImplementedError()
+
+    has_key = __contains__
+
+    def __getitem__(self, key):
+        self.raiseNotImplementedError()
+
+    def get(self, key, default=None):
+        if key in self:
+            return self[key]
+        return default
+
+    def __delitem__(self, key):
+        self.raiseNotImplementedError()
+
+    def pop(self, key, default=None):
+        value = default
+        if key in self:
+            value = self[key]
+            del self[key]
+        return value
+
+    def popitem(self):
+        self.raiseNotImplementedError()
+
+    def __iter__(self):
+        self.raiseNotImplementedError()
+
+    def iteritems(self):
+        self.raiseNotImplementedError()
+
+    def iterkeys(self):
+        self.raiseNotImplementedError()
+
+    def itervalues(self):
+        self.raiseNotImplementedError()
+
+    def viewitems(self):
+        self.raiseNotImplementedError()
+
+    def viewkeys(self):
+        self.raiseNotImplementedError()
+
+    def viewvalues(self):
+        self.raiseNotImplementedError()
+
+    def setdefault(self, value):
+        self.raiseNotImplementedError()
+
+    def update(self, other):
+        for key, value in other.items():
+            self[key] = value
+
+    def clear(self):
+        for key in self.keys():
+            del self[key]
+
+    def copy(self):
+        self.raiseNotImplementedError()
+
+
+# -------
+# Helpers
+# -------
 
 class dynamicProperty(object):
 
@@ -59,33 +149,65 @@ class dynamicProperty(object):
     it much easier for subclassing. Here is an example
     of why this is needed:
 
-    class BaseObject(object):
+        class BaseObject(object):
 
-        _foo = 1
+            _foo = 1
 
-        def _get_foo(self):
-            return self._foo
+            def _get_foo(self):
+                return self._foo
 
-        def _set_foo(self, value):
-            self._foo = value
+            def _set_foo(self, value):
+                self._foo = value
 
-    class MyObject(BaseObject):
+            foo = property(_get_foo, _set_foo)
 
-        def _set_foo(self, value):
-            self._foo = value * 100
 
-    >>> m = MyObject()
-    >>> m.foo
-    1
-    >>> m.foo = 2
-    >>> m.foo
-    2
+        class MyObject(BaseObject):
+
+            def _set_foo(self, value):
+                self._foo = value * 100
+
+
+        >>> m = MyObject()
+        >>> m.foo
+        1
+        >>> m.foo = 2
+        >>> m.foo
+        2
 
     The expected value is 200. The _set_foo method
     needs to be reregistered. Doing that also requires
     reregistering the _get_foo method. It's possible
     to do this, but it's messy and will make subclassing
     less than ideal.
+
+    Using dynamicProperty solves this.
+
+        class BaseObject(object):
+
+            _foo = 1
+
+            foo = dynamicProperty("foo")
+
+            def _get_foo(self):
+                return self._foo
+
+            def _set_foo(self, value):
+                self._foo = value
+
+
+        class MyObject(BaseObject):
+
+            def _set_foo(self, value):
+                self._foo = value * 100
+
+
+        >>> m = MyObject()
+        >>> m.foo
+        1
+        >>> m.foo = 2
+        >>> m.foo
+        200
     """
 
     __slots__ = [
