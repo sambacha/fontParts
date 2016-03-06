2c4e76d1a778a7045fc2e60b2c69fd5ddde2101a
Jack Jennings <jack@remote.gd>
2016-03-06 12:00:19 -0800

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup
+
+setup(
+    name='fontParts',
+    version="0.0.1",
+    author='Just van Rossum, Tal Leming, Erik van Blokland, others',
+    author_email='info@robofab.com',
+    packages=[
+        'fontParts',
+        'fontParts.objects',
+        'fontParts.objects.abstract',
+        'fontParts.objects.robofab'
+    ],
+    url='http://github.com/robofab-developers/fontParts',
+    license='LICENSE.txt',
+    description='Tools to manipulate font sources',
+    long_description='Tools to manipulate font sources',
+    install_requires=[],
+    package_dir={'': 'Lib'}
+)
