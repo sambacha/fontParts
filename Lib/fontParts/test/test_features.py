9ef4300a17bf5ace07cd09c52ff5ca39018c70e3
Tal Leming <tal@typesupply.com>
2016-06-12 18:56:17 -0400

    Test features.

--- Lib/fontParts/test/test_features.py
+++ Lib/fontParts/test/test_features.py
@@ -4,4 +4,30 @@ from fontParts.base import FontPartsError
 
 class TestFeatures(unittest.TestCase):
 
-    pass
+    def getFeatures_generic(self):
+        features, unreferenced = self.objectGenerator("features")
+        features.text = "# test"
+        return features, unreferenced
+
+    # ----
+    # Text
+    # ----
+
+    def test_text(self):
+        features, unreferenced = self.getFeatures_generic()
+        # get
+        self.assertEqual(
+            features.text,
+            "# test"
+        )
+        # set: valid
+        features.text = "# foo"
+        self.assertEqual(
+            features.text,
+            "# foo"
+        )
+        features.text = None
+        self.assertIsNone(features.text)
+        # set: invalid
+        with self.assertRaises(FontPartsError):
+            features.text = 123
