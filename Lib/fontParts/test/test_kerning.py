57136a2a96482ac0e488256d76324fce8fc1b082
Tal Leming <tal@typesupply.com>
2016-06-12 19:47:58 -0400

    "unrequested"

--- Lib/fontParts/test/test_kerning.py
+++ Lib/fontParts/test/test_kerning.py
@@ -5,21 +5,21 @@ from fontParts.base import FontPartsError
 class TestKerning(unittest.TestCase):
 
     def getKerning_generic(self):
-        kerning, unreferenced = self.objectGenerator("kerning")
+        kerning, unrequested = self.objectGenerator("kerning")
         kerning.update({
             ("public.kern1.X", "public.kern2.X") : 100,
             ("B", "public.kern2.X") : 101,
             ("public.kern1.X", "B") : 102,
             ("A", "A") : 103,
         })
-        return kerning, unreferenced
+        return kerning, unrequested
 
     # ---
     # len
     # ---
 
     def test_len(self):
-        kerning, unreferenced = self.getKerning_generic()
+        kerning, unrequested = self.getKerning_generic()
         self.assertEqual(
             len(kerning),
             4
