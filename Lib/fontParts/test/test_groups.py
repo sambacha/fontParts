353122c2ddf36b077470e20b7a9187f7284d7295
Tal Leming <tal@typesupply.com>
2016-06-12 19:53:00 -0400

    Test groups.

--- Lib/fontParts/test/test_groups.py
+++ Lib/fontParts/test/test_groups.py
@@ -4,4 +4,27 @@ from fontParts.base import FontPartsError
 
 class TestGroups(unittest.TestCase):
 
-    pass
+    def getGroups_generic(self):
+        groups, unrequested = self.objectGenerator("groups")
+        groups.update({
+            "group 1" : ["A", "B", "C"],
+            "group 2" : ["x", "y", "z"],
+            "group 3" : []
+        })
+        return groups, unrequested
+
+    # ---
+    # len
+    # ---
+
+    def test_len(self):
+        groups, unrequested = self.getGroups_generic()
+        self.assertEqual(
+            len(groups),
+            3
+        )
+        groups.clear()
+        self.assertEqual(
+            len(groups),
+            0
+        )
