ef7fcb3eb5f2d77afa58391bcee2529cbb440670
Tal Leming <tal@typesupply.com>
2016-06-12 19:45:54 -0400

    Test kerning.

--- Lib/fontParts/test/test_kerning.py
+++ Lib/fontParts/test/test_kerning.py
@@ -4,4 +4,28 @@ from fontParts.base import FontPartsError
 
 class TestKerning(unittest.TestCase):
 
-    pass
+    def getKerning_generic(self):
+        kerning, unreferenced = self.objectGenerator("kerning")
+        kerning.update({
+            ("public.kern1.X", "public.kern2.X") : 100,
+            ("B", "public.kern2.X") : 101,
+            ("public.kern1.X", "B") : 102,
+            ("A", "A") : 103,
+        })
+        return kerning, unreferenced
+
+    # ---
+    # len
+    # ---
+
+    def test_len(self):
+        kerning, unreferenced = self.getKerning_generic()
+        self.assertEqual(
+            len(kerning),
+            4
+        )
+        kerning.clear()
+        self.assertEqual(
+            len(kerning),
+            0
+        )
