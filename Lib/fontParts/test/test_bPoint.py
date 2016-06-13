a376e646dcd00d9419427309796b8f11aa2a3d98
Tal Leming <tal@typesupply.com>
2016-06-13 11:27:14 -0400

    Test bPoint.

--- Lib/fontParts/test/test_bPoint.py
+++ Lib/fontParts/test/test_bPoint.py
@@ -4,4 +4,22 @@ from fontParts.base import FontPartsError
 
 class TestBPoint(unittest.TestCase):
 
-    pass
+    def getBPoint_corner(self):
+        contour, unrequested = self.objectGenerator("contour")
+        unrequested.append(contour)
+        contour.appendPoint((0, 0), "move")
+        contour.appendPoint((101, 202), "line")
+        contour.appendPoint((303, 0), "line")
+        bPoint = contour.bPoints[1]
+        return bPoint, unrequested
+
+    # ----
+    # Type
+    # ----
+
+    def test_type(self):
+        bPoint, unrequested = self.getBPoint_corner()
+        self.assertEqual(
+            bPoint.type,
+            "corner"
+        )
