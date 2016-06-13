a6035ee673695382ea72df6a839ab56ba3444a39
Tal Leming <tal@typesupply.com>
2016-06-13 11:02:03 -0400

    Test point.

--- Lib/fontParts/test/test_point.py
+++ Lib/fontParts/test/test_point.py
@@ -4,4 +4,51 @@ from fontParts.base import FontPartsError
 
 class TestPoint(unittest.TestCase):
 
-    pass
+    def getPoint_generic(self):
+        contour, unrequested = self.objectGenerator("contour")
+        unrequested.append(contour)
+        contour.appendPoint((0, 0), "move")
+        contour.appendPoint((101, 202), "line")
+        point = contour.points[1]
+        return point, unrequested
+
+    # ----
+    # Type
+    # ----
+
+    def test_type(self):
+        point, unrequested = self.getPoint_generic()
+        # get
+        self.assertEqual(
+            point.type,
+            "line"
+        )
+        # set: move
+        point.type = "move"
+        self.assertEqual(
+            point.type,
+            "move"
+        )
+        # set: curve
+        point.type = "curve"
+        self.assertEqual(
+            point.type,
+            "curve"
+        )
+        # set: qcurve
+        point.type = "qcurve"
+        self.assertEqual(
+            point.type,
+            "qcurve"
+        )
+        # set: offcurve
+        point.type = "offcurve"
+        self.assertEqual(
+            point.type,
+            "offcurve"
+        )
+        # set: invalid
+        with self.assertRaises(FontPartsError):
+            point.type = "xxx"
+        with self.assertRaises(FontPartsError):
+            point.type = 123
