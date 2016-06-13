74120e3344da14c2e25ecfa2a2ad816491ccedf4
Tal Leming <tal@typesupply.com>
2016-06-13 11:55:34 -0400

    Test info.

--- Lib/fontParts/nonelab/info.py
+++ Lib/fontParts/nonelab/info.py
@@ -5,6 +5,8 @@ from fontParts.nonelab.base import RBaseObject
 
 class RInfo(RBaseObject, BaseInfo):
 
+    wrapClass = defcon.Info
+
     def _getAttr(self, attr):
         return getattr(self.naked(), attr)
 
--- Lib/fontParts/test/test_info.py
+++ Lib/fontParts/test/test_info.py
@@ -4,4 +4,35 @@ from fontParts.base import FontPartsError
 
 class TestInfo(unittest.TestCase):
 
-    pass
+    def getInfo_generic(self):
+        info, unrequested = self.objectGenerator("info")
+        info.unitsPerEm = 1000
+        return info, unrequested
+
+    # ----------
+    # Dimensions
+    # ----------
+
+    def test_unitsPerEm(self):
+        info, unrequested = self.getInfo_generic()
+        # get
+        self.assertEqual(
+            info.unitsPerEm,
+            1000
+        )
+        # set: valid
+        info.unitsPerEm = 2000
+        self.assertEqual(
+            info.unitsPerEm,
+            2000
+        )
+        info.unitsPerEm = 2000.1
+        self.assertEqual(
+            info.unitsPerEm,
+            2000.1
+        )
+        # set: invalid
+        with self.assertRaises(FontPartsError):
+            info.unitsPerEm = -1000
+        with self.assertRaises(FontPartsError):
+            info.unitsPerEm = "abc"
