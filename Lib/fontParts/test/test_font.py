2bf932ccefbbb8a35fc72cc3b4337d661f17c5df
Tal Leming <tal@typesupply.com>
2016-06-13 07:41:43 -0400

    Test font.

--- Lib/fontParts/test/test_font.py
+++ Lib/fontParts/test/test_font.py
@@ -4,4 +4,29 @@ from fontParts.base import FontPartsError
 
 class TestFont(unittest.TestCase):
 
-    pass
+    # ------
+    # Glyphs
+    # ------
+
+    def getFont_glyphs(self):
+        font, unrequested = self.objectGenerator("font")
+        for name in "ABCD":
+            glyph = font.newGlyph(name)
+        return font, unrequested
+
+    # len
+
+    def test_len(self):
+        font, unrequested = self.getFont_glyphs()
+        # one layer
+        self.assertEqual(
+            len(font),
+            4
+        )
+        # two layers
+        layer = font.newLayer("test")
+        layer.newGlyph("X")
+        self.assertEqual(
+            len(font),
+            4
+        )
