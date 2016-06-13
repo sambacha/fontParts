99f6f504a0ca0c0bc4d112b2932c70cc7c2de19b
Tal Leming <tal@typesupply.com>
2016-06-12 20:57:38 -0400

    Test guideline.

--- Lib/fontParts/test/test_guideline.py
+++ Lib/fontParts/test/test_guideline.py
@@ -4,4 +4,50 @@ from fontParts.base import FontPartsError
 
 class TestGuideline(unittest.TestCase):
 
-    pass
+    def getGuideline_generic(self):
+        guideline, unrequested = self.objectGenerator("guideline")
+        guideline.x = 1
+        guideline.y = 2
+        guideline.angle = 90
+        return guideline, unrequested
+
+    # --------
+    # Position
+    # --------
+
+    def test_x(self):
+        guideline, unrequested = self.getGuideline_generic()
+        # get
+        self.assertEqual(
+            guideline.x,
+            1
+        )
+        # set: valid
+        guideline.x = 0
+        self.assertEqual(
+            guideline.x,
+            0
+        )
+        guideline.x = 1
+        self.assertEqual(
+            guideline.x,
+            1
+        )
+        guideline.x = -1
+        self.assertEqual(
+            guideline.x,
+            -1
+        )
+        guideline.x = 1.1
+        self.assertEqual(
+            guideline.x,
+            1.1
+        )
+        guideline.x = -1.1
+        self.assertEqual(
+            guideline.x,
+            -1.1
+        )
+        # set: invalid
+        with self.assertRaises(FontPartsError):
+            guideline.x = "ABC"
