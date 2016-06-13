cf9fc779e0d7d93d3191e8881d4667c701578001
Tal Leming <tal@typesupply.com>
2016-06-13 07:50:01 -0400

    Test layer.

--- Lib/fontParts/test/test_layer.py
+++ Lib/fontParts/test/test_layer.py
@@ -4,4 +4,21 @@ from fontParts.base import FontPartsError
 
 class TestLayer(unittest.TestCase):
 
-    pass
+    # ------
+    # Glyphs
+    # ------
+
+    def getLayer_glyphs(self):
+        layer, unrequested = self.objectGenerator("layer")
+        for name in "ABCD":
+            glyph = layer.newGlyph(name)
+        return layer, unrequested
+
+    # len
+
+    def test_len(self):
+        layer, unrequested = self.getLayer_glyphs()
+        self.assertEqual(
+            len(layer),
+            4
+        )
