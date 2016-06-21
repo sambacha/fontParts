d1476d6d3603a2cf3b2b83300d8387ef21566f79
Ben Kiel <ben@typefounding.com>
2016-06-21 11:12:44 -0500

    Cleaning up the Color doc string.

--- Lib/fontParts/base/color.py
+++ Lib/fontParts/base/color.py
@@ -1,21 +1,25 @@
 class Color(tuple):
 
+    """
+    An color object. This follows the :ref:`type-color`.
+    """
+
     def _get_r(self):
         return _stringToSequence(self)[0]
 
-    r = property(_get_r, "The red component.")
+    r = property(_get_r, "The color's red component as :ref:`type-int-float`.")
 
     def _get_g(self):
         return _stringToSequence(self)[1]
 
-    g = property(_get_g, "The green component.")
+    g = property(_get_g, "The color's green component as :ref:`type-int-float`.")
 
     def _get_b(self):
         return _stringToSequence(self)[2]
 
-    b = property(_get_b, "The blue component.")
+    b = property(_get_b, "The color's blue component as :ref:`type-int-float`.")
 
     def _get_a(self):
         return _stringToSequence(self)[3]
 
-    a = property(_get_a, "The alpha component.")
+    a = property(_get_a, "The color's alpha component as :ref:`type-int-float`.")
