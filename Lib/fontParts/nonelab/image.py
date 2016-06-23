d85657a0e56e0e93dddc74ef8e9bd3b0cd223a08
Ben Kiel <ben@typefounding.com>
2016-06-23 14:06:12 -0500

    over eager find/replace

--- Lib/fontParts/nonelab/image.py
+++ Lib/fontParts/nonelab/image.py
@@ -50,7 +50,7 @@ class RImage(RBaseObject, BaseImage):
         return images[fileName]
 
     def _set_data(self, value):
-        from ufoLib.normalizers import pngValidator
+        from ufoLib.validators import pngValidator
         if not isinstance(value, bytes):
             raise FontPartsError("The image data provided is not valid.")
         if not pngValidator(data=value):
