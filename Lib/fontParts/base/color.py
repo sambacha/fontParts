bdce3bc3c6735bd0989a7d1eef9418f8332e6d27
Ben Kiel <ben@typefounding.com>
2016-06-23 14:22:59 -0500

    It seems that the _stringToSequence method was missing. @typesupply can you check this?

--- Lib/fontParts/base/color.py
+++ Lib/fontParts/base/color.py
@@ -23,3 +23,17 @@ class Color(tuple):
         return _stringToSequence(self)[3]
 
     a = property(_get_a, "The color's alpha component as :ref:`type-int-float`.")
+    
+    def _stringToSequence(value):
+        r, g, b, a = [i.strip() for i in value.split(",")]
+        value = []
+        for component in (r, g, b, a):
+            try:
+                v = int(component)
+                value.append(v)
+                continue
+            except ValueError:
+                pass
+            v = float(component)
+            value.append(v)
+        return value
