f2739eee9af9070205d28ecd17a76e9f32c63ff6
Frederik Berlaen <frederik@typemytype.com>
2017-08-18 17:26:04 +0200

    remove the negative glyph width limitation
    see
    https://github.com/robofab-developers/fontParts/issues/73#issuecomment-3
    23378035

--- Lib/fontParts/base/normalizers.py
+++ Lib/fontParts/base/normalizers.py
@@ -283,13 +283,10 @@ def normalizeGlyphWidth(value):
     Normalizes glyph width.
 
     * **value** must be a :ref:`type-int-float`.
-    * **value** cannot be negative.
     * Returned value is the same type as the input value.
     """
     if not isinstance(value, (int, float)):
         raise FontPartsError("Glyph width must be an :ref:`type-int-float`, not %s." % type(value).__name__)
-    if value < 0:
-        raise FontPartsError("Glyph width must be be 0 or greater.")
     return value
 
 
