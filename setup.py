d46967076cef3843e8a8aceea01cff14cc0ec5bd
Ben Kiel <ben@typefounding.com>
2018-03-26 07:58:34 -0400

    Update defcon

--- setup.py
+++ setup.py
@@ -159,7 +159,7 @@ setup_params = dict(
         "fonttools>=3.20.0",
         "ufoLib>=2.0",
         "fontMath>=0.4.4",
-        "defcon[pens]>=0.3.6",
+        "defcon[pens]>=0.4.0",
     ],
     cmdclass={
         "release": release,
