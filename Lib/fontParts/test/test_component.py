6d654b8ca2b5a7dc3adf2a7231deb06840a223c8
Ben Kiel <ben@typefounding.com>
2018-04-04 10:42:44 -0500

    Putting back in getting of identifier

--- Lib/fontParts/test/test_component.py
+++ Lib/fontParts/test/test_component.py
@@ -486,6 +486,7 @@ class TestComponent(unittest.TestCase):
         from .legacyPointPen import LegacyPointPen
         component = self.getComponent_generic()
         component.transformation = (1, 2, 3, 4, 5, 6)
+        component.getIdentifier()
         pointPen = LegacyPointPen()
         component.drawPoints(pointPen)
         expected = [('addComponent', (u'A', (1.0, 2.0, 3.0, 4.0, 5.0, 6.0)), {})]
