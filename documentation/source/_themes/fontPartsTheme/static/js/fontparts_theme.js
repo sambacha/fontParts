b712c165e78c3dbd0e549127efab8fe2e471926b
Stephen Nixon <swr410@gmail.com>
2018-08-30 21:13:29 -0500

    add slide-out nav for mobile

--- documentation/source/_themes/fontPartsTheme/static/js/fontparts_theme.js
+++ documentation/source/_themes/fontPartsTheme/static/js/fontparts_theme.js
@@ -1,1 +1,27 @@
 console.log("hello fontparts")
+
+// const sidebar = document.querySelector("#sidebar")
+
+// console.log(sidebar.classList)
+
+function collapseSidebar() {
+    var sidebar = document.querySelector('#sidebar');
+    
+    if (sidebar != null) {
+        sidebar.classList.add('collapsed')
+    }
+};
+
+window.addEventListener('DOMContentLoaded', function() {
+    console.log('window - DOMContentLoaded - capture');
+    collapseSidebar()
+
+    var navButton = document.querySelector('#nav-button');
+    navButton.addEventListener('click', toggleNav)
+}, true);
+
+function toggleNav() {
+    console.log('nav icon clicked');
+    var sidebar = document.querySelector('#sidebar');
+    sidebar.classList.toggle('expanded')
+}
